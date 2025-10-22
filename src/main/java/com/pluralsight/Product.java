9d2865076f061472c67b214f54362e8184570408
Qi Qing <learn.qiqing@gmail.com>
2025-10-22 18:46:39 -0400

    added Product class: attributes, constructor, getters and toString.

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
 
 public class Product {
+    private String sku;
+    private String productName;
+    private double price;
+
+
+    public Product(String sku, String productName, double price) {
+        this.sku = sku;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "sku='" + sku + '\'' +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
